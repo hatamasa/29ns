6ac8936b3b268b61b6d2b7d8c2b4edd9161b6e69
hatamasa <masaya19880517@gmail.com>
2019-05-24 00:06:36 +0900

    ログイン周りを作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,55 @@
 @extends('layouts.app')
 
+@section('style')
+<link href="{{ asset('css/auth.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div>
+    <div class="content-head">
+        <div class="page-title">ログイン</div>
+    </div>
+    <div class="content-body">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group">
+                <input id="email" type="email"
+                    class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                    name="email" value="{{ old('email') }}" required autofocus placeholder="メールアドレス">
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <div class="form-group">
+                <input id="password" type="password"
+                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                    name="password" required placeholder="パスワード">
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+            <div class="form-group form-remenber">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember"{{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label" for="remember">ログインしたままにする</label>
                 </div>
             </div>
-        </div>
+
+            <div class="form-group form-login">
+                <button type="submit" class="btn btn-primary btn-block">ログイン</button>
+                <a class="btn btn-info btn-block" href="{{ route('register') }}">新規会員登録はこちら</a>
+                @if (Route::has('password.request'))
+                    <a class="btn btn-link" href="{{ route('password.request') }}">パスワードを忘れた方はこちら</a>
+                @endif
+            </div>
+        </form>
+
     </div>
 </div>
 @endsection
