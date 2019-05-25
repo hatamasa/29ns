2c9ea33146b6fcd8523e2d89933ec10e80f8bea2
hatamasa <masaya19880517@gmail.com>
2019-05-25 13:53:01 +0900

    posts作成で店舗カテゴリを絞ってなかったので修正

---
+++ config/const.php
@@ -0,0 +1,102 @@
+<?php
+
+return [
+    // ぐるなびAPI
+    'gnavi' => [
+        'category' => [
+            'yakiniku' => [
+                [
+                    "category_s_code" => "RSFST05001",
+                    "category_s_name" => "焼肉",
+                    "category_l_code" => "RSFST05000"
+                ],
+                [
+                    "category_s_code" => "RSFST05004",
+                    "category_s_name" => "サムギョプサル",
+                    "category_l_code" => "RSFST05000"
+                ],
+                [
+                    "category_s_code" => "RSFST05002",
+                    "category_s_name" => "ホルモン",
+                    "category_l_code" => "RSFST05000"
+                ],
+                [
+                    "category_s_code" => "RSFST05003",
+                    "category_s_name" => "ジンギスカン",
+                    "category_l_code" => "RSFST05000"
+                ],
+            ],
+            'yakitori' => [
+                [
+                    "category_s_code" => "RSFST06003",
+                    "category_s_name" => "焼き鳥",
+                    "category_l_code" => "RSFST06000"
+                ],
+                [
+                    "category_s_code" => "RSFST06004",
+                    "category_s_name" => "鶏料理(鳥料理)",
+                    "category_l_code" => "RSFST06000"
+                ],
+                [
+                    "category_s_code" => "RSFST06012",
+                    "category_s_name" => "串焼き",
+                    "category_l_code" => "RSFST06000"
+                ],
+                [
+                    "category_s_code" => "RSFST06018",
+                    "category_s_name" => "鴨料理",
+                    "category_l_code" => "RSFST06000"
+                ]
+            ],
+            'steak' => [
+                [
+                    "category_s_code" => "RSFST06009",
+                    "category_s_name" => "ステーキ",
+                    "category_l_code" => "RSFST06000"
+                ],
+            ],
+            'hamburg' => [
+                [
+                    "category_s_code" => "RSFST06010",
+                    "category_s_name" => "ハンバーグ",
+                    "category_l_code" => "RSFST06000"
+                ],
+            ],
+            'hamburger' => [
+                [
+                    "category_s_code" => "RSFST06011",
+                    "category_s_name" => "ハンバーガー",
+                    "category_l_code" => "RSFST18000"
+                ],
+            ],
+            'bar' => [
+                [],
+            ],
+            'other' => [
+                [
+                    "category_s_code" => "RSFST06001",
+                    "category_s_name" => "牛タン",
+                    "category_l_code" => "RSFST06000"
+                ],
+                [
+                    "category_s_code" => "RSFST06006",
+                    "category_s_name" => "鉄板焼き",
+                    "category_l_code" => "RSFST06000"
+                ],
+                [
+                    "category_s_code" => "RSFST06013",
+                    "category_s_name" => "豚肉料理",
+                    "category_l_code" => "RSFST06000"
+                ],
+                [
+                    "category_s_code" => "RSFST06014",
+                    "category_s_name" => "馬肉料理",
+                    "category_l_code" => "RSFST06000"
+                ],
+            ],
+        ],
+    ],
+
+
+
+];
--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Services\ApiService;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 
@@ -18,22 +19,37 @@ class PostsSeeder extends Seeder
 
         $faker = Faker::create('ja_JP');
 
+        $category = [
+            Config::get("const.gnavi.category.yakiniku")[0]['category_s_code'],
+            Config::get("const.gnavi.category.yakiniku")[1]['category_s_code'],
+            Config::get("const.gnavi.category.yakitori")[0]['category_s_code'],
+            Config::get("const.gnavi.category.yakitori")[1]['category_s_code'],
+            Config::get("const.gnavi.category.steak")[0]['category_s_code'],
+            Config::get("const.gnavi.category.hamburg")[0]['category_s_code'],
+            Config::get("const.gnavi.category.hamburger")[0]['category_s_code'],
+            Config::get("const.gnavi.category.other")[0]['category_s_code'],
+            Config::get("const.gnavi.category.other")[1]['category_s_code'],
+            Config::get("const.gnavi.category.other")[2]['category_s_code'],
+        ];
+
         $options = [
             // 田町, 三田
-            'areacode_m' => 'AREAM2175',
+            'areacode_m'   => 'AREAM2175',
+            'category_s'   => implode(',', $category),
+            'hit_per_page' => 100
         ];
         $result = (new ApiService())->callGnaviRestSearchApi($options);
 
         $user_ids  = DB::table('Users')->pluck('id');
 
         $data = [];
-        for($i = 0; $i < 100; $i++){
+        for($i = 0; $i < 1000; $i++){
             $data[] = [
                 'user_id'       => $faker->randomElement($user_ids),
                 'shop_cd'       => $faker->randomElement($result['rest'])['id'],
                 'score'         => $faker->numberBetween(0, 10),
                 'visit_count'   => $faker->numberBetween(1, 5),
-                'title'         => $faker->realText(30),
+                'title'         => $faker->realText(100),
                 'contents'      => $faker->realText(),
                 'img_url_1'     => $faker->imageUrl(),
                 'img_url_2'     => $faker->imageUrl(),
