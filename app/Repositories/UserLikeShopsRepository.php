5adf2db1be7030195a2b7c54f6d432f914f18020
hatamasa <masaya19880517@gmail.com>
2019-07-22 21:58:27 +0900

    肉ログに件数とお気に入り数が出ていなかったので修正

--- app/Repositories/UserLikeShopsRepository.php
+++ app/Repositories/UserLikeShopsRepository.php
@@ -16,9 +16,17 @@ class UserLikeShopsRepository
      */
     public function getListByUserId(int $limit, $page, int $user_id)
     {
-        $query = DB::table('user_like_shops')
-            ->where('user_id', $user_id)
-            ->orderBy('created_at', 'desc')
+        $query = DB::table('user_like_shops as uls')
+            ->select(
+                's.shop_cd',
+                's.score',
+                's.post_count',
+                's.like_count',
+                'uls.user_id'
+            )
+            ->join('shops as s', 'uls.shop_cd', '=', 's.shop_cd')
+            ->where('uls.user_id', $user_id)
+            ->orderBy('uls.created_at', 'desc')
             ->offset(($page-1) * $limit)
             ->limit($limit)
         ;
