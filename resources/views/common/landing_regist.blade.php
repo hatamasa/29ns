3a79d36b77e9d81d60be273edae808f52f50e90f
hatamasa <masaya19880517@gmail.com>
2019-08-20 00:44:32 +0900

    レビューの文言を修正

--- resources/views/common/landing_regist.blade.php
+++ resources/views/common/landing_regist.blade.php
@@ -1,5 +1,5 @@
 @guest
 <div class="form-group">
-    <a class="register btn btn-info btn-block" href="{{ route('register') }}">お店のレビューやお気に入りができる！<br>無料会員登録はこちら</a>
+    <a class="register btn btn-info btn-block" href="{{ route('register') }}">お店の口コミやお気に入りができる！<br>無料会員登録はこちら</a>
 </div>
 @endguest
