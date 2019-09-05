46ed9e11a429f83a29598944ed69488dea9d20dc
hatamasa <masaya19880517@gmail.com>
2019-09-05 23:22:08 +0900

    店舗登録のバッチを作成

--- app/Console/Commands/InsertShopsCommand.php
+++ app/Console/Commands/InsertShopsCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use App\Services\ApiService;
 
@@ -46,30 +47,35 @@ class InsertShopsCommand extends Command
 
         DB::beginTransaction();
         try {
-            $page = 0;
-            do {
-                $page++;
-                $options = [
-                    "hit_per_page" => 100,
-                    "offset_page" => 1+100*($page-1),
-                ];
+            foreach (Config::get("const.area_l") as $key => $val) {
+                $this->info($key." ".$val);
+                $page = 0;
+                do {
+                    $page++;
+                    $options = [
+                        "hit_per_page" => 100,
+                        "offset_page" => $page,
+                        "areacode_l" => $key
+                    ];
 
-                $result = $this->ApiService->callGnaviRestSearchApi($options);
-                foreach ($result['rest'] as &$shop) {
-                    DB::table("shops")->updateOrInsert(
-                        [
-                            "shop_cd" => $shop["id"],
-                        ],[
-                            "shop_cd" => $shop["id"],
-                        ]
-                    );
-                }
-                $this->info("page: ".$page."/".ceil($result['total_hit_count']/100));
-            } while(ceil($result['total_hit_count']/100) > $page);
+                    $result = $this->ApiService->callGnaviRestSearchApi($options, ApiService::CATEGORY_YAKINIKU);
+                    foreach ($result['rest'] as $shop) {
+                        DB::table("shops")->updateOrInsert(
+                            [
+                                "shop_cd" => $shop["id"],
+                            ],[
+                                "shop_cd" => $shop["id"],
+                            ]
+                        );
+                    }
+                    $this->info("page: ".$page."/".ceil($result['total_hit_count']/100));
+                } while(ceil($result['total_hit_count']/100) > $page);
+            }
             DB::commit();
         } catch (\Exception $e) {
             DB::rollBack();
             $this->info("error!!");
+            $this->info($e->getTrace());
             return;
         }
 
