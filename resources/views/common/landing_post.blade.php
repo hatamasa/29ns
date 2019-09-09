2a80c0936bd2ba83f345c02c7683dd7d24c68120
hatamasa <masaya19880517@gmail.com>
2019-09-09 21:31:10 +0900

    LPの駅から探すパスが間違っていたので修正

--- resources/views/common/landing_post.blade.php
+++ resources/views/common/landing_post.blade.php
@@ -11,7 +11,7 @@
     </div>
 
     <div class="form-group search-btn">
-        <a href="{{ url('/search/station') }}" class="btn btn-default btn-lg" role="button">駅から<br>探す</a>
+        <a href="{{ url('/search/line_company') }}" class="btn btn-default btn-lg" role="button">駅から<br>探す</a>
         <a href="{{ url('/search/area') }}" class="btn btn-default btn-lg">エリアから<br>探す</a>
         {{--<a href="{{ url('/shops/near') }}" class="btn btn-default btn-lg">近くのお店を<br>探す</a>--}}
     </div>
